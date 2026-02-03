41e31e6d8588eb0ac22bdafb9997e1cecbba29ea
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-02-02 22:59:24 -0300

    Bora la Estudar Angular no Frontend_2026

--- ProjetoAngularALKTECNO/projeto-components/src/app/app.ts
+++ ProjetoAngularALKTECNO/projeto-components/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('projetos_Componentes_inicial--no-standalone');
+  protected readonly title = signal('projeto-components');
 }
--- ProjetoAngularALKTECNO/projeto-components/src/app/card/card.ts
+++ ProjetoAngularALKTECNO/projeto-components/src/app/card/card.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './card.scss',
 })
 export class Card {
-  
+
 }
